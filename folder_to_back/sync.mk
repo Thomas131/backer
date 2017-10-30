4910d4af9a7c87347f53cf294f20900ca112c0f4
backer@nas_qnap <t@t131.us.to>
2017-10-31 00:09:23 +0100

    V2.1

---
+++ folder_to_back/sync.mk
@@ -0,0 +1,6 @@
+check_backthis:
+	#@TODO: Check .backupthis
+
+sync_back:
+	rsync -a --delete --numeric-ids --delete-excluded -u -x --password-file=../.passwordfile --stats -h hostname::folder_to_back back.0 #in this case a rsync deamon ... faster because no encryption ... use just at home!
+	touch back.0
--- sync.mk
+++
@@ -1,4 +0,0 @@
-sync_back:
-	rsync -a -z --delete --numeric-ids --relative --delete-excluded user@host:/path/to/back/ back.0
-	rsync -a -z --delete --numeric-ids --relative --delete-excluded user@host:/secound/path/ back.0
-	touch back.0
