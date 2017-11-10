24b2c6bbc7c857490632510964293838431129ff
backer@nas_qnap <t@t131.us.to>
2017-11-10 12:52:31 +0100

    Addded -D flag to skip device and special files

--- folder_to_back/sync.mk
+++ folder_to_back/sync.mk
@@ -2,5 +2,5 @@ check_backthis:
 	#@TODO: Check .backupthis
 
 sync_back:
-	time rsync -a --delete --numeric-ids --delete-excluded -u -x --password-file=../.passwordfile --stats -h hostname::folder_to_back back.0 #in this case a rsync deamon ... faster because no encryption ... use just at home!
+	time rsync -a -D --delete --numeric-ids --delete-excluded -u -x --password-file=../.passwordfile --stats -h hostname::folder_to_back back.0 #in this case a rsync deamon ... faster because no encryption ... use just at home!
 	touch back.0
