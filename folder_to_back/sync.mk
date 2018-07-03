7a0a5e8ce62c82b8c80519cbc712ac8710474b00
backer@nas_qnap <t@t131.us.to>
2018-07-03 12:23:30 +0200

    added Hooks before and after Backup

--- folder_to_back/sync.mk
+++ folder_to_back/sync.mk
@@ -1,6 +1,11 @@
+hook_before_back:
+
 check_backthis:
 	#@TODO: Check .backupthis
 
 sync_back:
 	time rsync -a --no-specials --no-devices --delete --numeric-ids --delete-excluded -u -x --password-file=../.passwordfile --stats -h hostname::folder_to_back pending #in this case a rsync deamon ... faster because no encryption ... use just at home!
 	touch pending
+
+hook_after_back:
+
